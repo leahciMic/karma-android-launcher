1b0d8816b8dde1dcb37cd6bd7ec3d1a68499f1b3
Michael Leaney <mleaney@tokbox.com>
2015-09-16 09:48:58 +1000

    Use androidctrl

--- index.js
+++ index.js
@@ -1,98 +1,35 @@
-var spawn = require('child_process').spawn;
 var bluebird = require('bluebird');
-var split = require('split');
 var debug = require('debug')('karma-android-launcher:debug');
 var warn = require('debug')('karma-android-launcher:warn');
-var ezspawn = require('ezspawn');
+var androidCtrl = require('androidctrl');
+var thenerize = require('thenerize');
 
-var spawnAndWaitFor = function(cmd, regex) {
-  debug('launching ' + cmd + ' and waiting for output that matches ' + regex);
-  return ezspawn(cmd, false)
-    .then(function(proc) {
-      return new bluebird.Promise(function(resolve, reject) {
-        proc.stdout.pipe(split())
-          .on('data', function(data) {
-            if (data.match(regex)) {
-              resolve();
-            }
-          });
-      });
-    });
-};
+thenerize(androidCtrl);
 
 var AndroidBrowser = function(baseBrowserDecorator, script, args) {
   baseBrowserDecorator(this);
 
   var self = this;
 
-  if (args.shutdownEmulator === undefined) {
-    args.shutdownEmulator = true;
-  }
-
-  if (!args.shutdownEmulator) {
-    // do not shutdown the emulator when finished
-    // handy if you're running more than one browser or test and do not wish
-    // to wait for the emulator to bootup each time
-    var killingPromise;
-
-    debug('Disabling kill methods');
-
-    self.kill = function() {
-      if (killingPromise) {
-        return killingPromise;
-      }
-
-      killingPromise = bluebird.resolve().then(function() {
-        self.state = self.FINISHED;
-      });
-
-      self.state = self.BEING_KILLED;
-      return killingPromise;
-    };
-
-    self.forceKill = function() {
-      self.kill();
-      self.state = self.BEING_FORCE_KILLED;
-      return killingPromise;
-    };
-  }
-
+  self.deviceId = undefined;
   self.name = script;
 
   self._start = function(url) {
-    debug('Starting browser');
-    ezspawn('adb -e devices').then(function(re) {
-      if (!re.stdout.match(/emulator-\d+/)) {
-        // she's not running :()
-        self._execCommand(self._getCommand(), self._getOptions(url));
-      }
-    })
-      .then(function() {
-        return ezspawn('adb -e wait-for-device');
-      })
-      .then(function() {
-        return spawnAndWaitFor('adb -e logcat', /Boot is finished/);
-      })
-      .then(function() {
-        return ezspawn('adb shell input keyevent 26');
-      })
-      .then(function() {
-        return ezspawn('adb shell input keyevent 82');
-      })
-      .then(function() {
-        return ezspawn('adb shell am start -a android.intent.action.VIEW -n ' + self.className + ' -d ' + url);
-      });
+    var deviceStarted = androidCtrl.startOrCreate();
+    var deviceId = deviceStarted.then(function(device) {
+      return device.id;
+    });
+    return deviceId.then(function(deviceID) {
+      self.deviceId = deviceID;
+      return androidCtrl.ensureReady(deviceID)
+        .then(androidCtrl.thenPowerOn(self.deviceId))
+        .then(androidCtrl.thenUnlock(self.deviceId))
+        .then(androidCtrl.thenAdb(
+          self.deviceId,
+          'shell am start -a android.intent.action.VIEW -n ' + self.className + ' -d ' + url
+        ));
+    });
   };
-
-  self._getCommand = function() {
-    return 'emulator';
-  };
-
-  self._getOptions = function(url) {
-    return [
-      '-avd', 'Android511'
-    ];
-  }
 };
 
 AndroidBrowser.$inject = ['baseBrowserDecorator', 'name', 'args'];
