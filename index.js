c8e9870db744cc60206e2d561d6e0927a39fb521
Michael Leaney <mleaney@tokbox.com>
2015-09-16 22:15:44 +1000

    Install the browser if it's not already

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var bluebird = require('bluebird');
+var Promise = require('bluebird');
 var debug = require('debug')('karma-android-launcher:debug');
 var warn = require('debug')('karma-android-launcher:warn');
 var androidCtrl = require('androidctrl');
 var thenerize = require('thenerize');
+var persistRequest = require('persist-request')(os.tmpdir());
+var fs = require('fs');
+var tmp = require('tmp');
+var path = require('path');
+
+var CHROME_APK_URL = 'http://mars.androidapksfree.com/files/pluto/com.android.chrome-v44.0.2403.128-240312811-x86-Android-5.0.apk';
+var FIREFOX_APK_URL = 'https://ftp.mozilla.org/pub/mozilla.org/mobile/releases/latest/android-x86/en-US/fennec-40.0.en-US.android-i386.apk';
 
 thenerize(androidCtrl);
 
@@ -20,10 +27,30 @@ var AndroidBrowser = function(baseBrowserDecorator, script, args) {
       return device.id;
     });
     return deviceId.then(function(deviceID) {
+      var packageName = self.className.split('/')[0];
+      var apkURL = (packageName === 'com.android.chrome' ?
+        CHROME_APK_URL :
+        FIREFOX_APK_URL
+      );
+
       self.deviceId = deviceID;
+
       return androidCtrl.ensureReady(deviceID)
-        .then(androidCtrl.thenPowerOn(self.deviceId))
-        .then(androidCtrl.thenUnlock(self.deviceId))
+        .then(androidCtrl.thenPowerOn(deviceID))
+        .then(androidCtrl.thenUnlock(deviceID))
+        .then(androidCtrl.thenIsInstalled(packageName))
+        .then(function(isInstalled) {
+          if (isInstalled) {
+            return;
+          }
+
+          return new Promise(function(resolve) {
+            var stream = persistRequest(apkURL);
+            stream.on('finish', function() {
+              resolve(stream.filename);
+            });
+          }).then(androidCtrl.install);
+        })
         .then(androidCtrl.thenAdb(
           self.deviceId,
           'shell am start -a android.intent.action.VIEW -n ' + self.className + ' -d ' + url
