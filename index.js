c3165d022a16ab789618b37a631d2fe9c5398a47
Michael Leaney <mleaney@tokbox.com>
2015-09-21 14:50:11 +1000

    Remove thenerize and use promise-preserve

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ var debug = require('debug')('karma-android-launcher:debug');
 var warn = require('debug')('karma-android-launcher:warn');
 var verbose = require('debug')('karma-android-launcher:verbose');
 var androidCtrl = require('androidctrl');
-var thenerize = require('thenerize');
 var os = require('os');
 var persistRequest = require('persist-request')(os.tmpdir());
 var fs = require('fs');
 var tmp = require('tmp');
 var path = require('path');
+var preserve = require('promise-preserve');
 
 var CHROME_APK_URL = 'http://mars.androidapksfree.com/files/pluto/com.android.chrome-v44.0.2403.128-240312811-x86-Android-5.0.apk';
 var FIREFOX_APK_URL = 'https://ftp.mozilla.org/pub/mozilla.org/mobile/releases/latest/android-x86/en-US/fennec-40.0.en-US.android-i386.apk';
-
-thenerize(androidCtrl);
 
 var AndroidBrowser = function(baseBrowserDecorator, script, args) {
   baseBrowserDecorator(this);
@@ -46,42 +44,48 @@ var AndroidBrowser = function(baseBrowserDecorator, script, args) {
     var deviceStarted = androidCtrl.startOrCreate({
       'hw.gpu.enabled': 'yes'
     });
+
     var deviceId = deviceStarted.then(function(device) {
       return device.id;
     });
 
-    return deviceId.then(function(deviceID) {
-      var packageName = self.className.split('/')[0];
-      var apkURL = (packageName === 'com.android.chrome' ?
-        CHROME_APK_URL :
-        FIREFOX_APK_URL
-      );
+    var packageName = self.className.split('/')[0];
+    var apkURL = (packageName === 'com.android.chrome' ?
+      CHROME_APK_URL :
+      FIREFOX_APK_URL
+    );
 
-      self.deviceId = deviceID;
+    return deviceId
+      .then(function(deviceId) {
+        self.deviceId = deviceId;
+        return deviceId;
+      })
+      .then(preserve(androidCtrl, 'ensureReady'))
+      .then(preserve(androidCtrl, 'powerOn'))
+      .then(preserve(androidCtrl, 'unlock'))
+      .then(preserve(function(deviceId) {
+        return androidCtrl.isInstalled(deviceId, packageName)
+          .then(function(isInstalled) {
+            if (isInstalled) {
+              debug('already installed');
+              return;
+            }
 
-      return androidCtrl.ensureReady(deviceID)
-        .then(androidCtrl.thenPowerOn(deviceID))
-        .then(androidCtrl.thenUnlock(deviceID))
-        .then(androidCtrl.thenIsInstalled(deviceID, packageName))
-        .then(function(isInstalled) {
-          if (isInstalled) {
-            debug('already installed');
-            return;
-          }
-
-          return new Promise(function(resolve) {
-            debug('getting ' + apkURL);
-            var stream = persistRequest.get(apkURL);
-            stream.on('cacheFile', function(filename) {
-              resolve(filename);
-            });
-          }).then(androidCtrl.install.bind(androidCtrl, deviceID));
-        })
-        .then(androidCtrl.thenAdb(
-          self.deviceId,
+            return new Promise(function(resolve) {
+              debug('getting ' + apkURL);
+              var stream = persistRequest.get(apkURL);
+              stream.on('cacheFile', function(filename) {
+                resolve(filename);
+              });
+            }).then(androidCtrl.install.bind(androidCtrl, deviceId));
+          });
+      }))
+      .then(function(deviceId) {
+        return androidCtrl.adb(
+          deviceId,
           'shell am start -a android.intent.action.VIEW -n ' + self.className + ' -d ' + url
-        ));
-    });
+        );
+      });
   };
 };
 
